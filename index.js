cbce605df3250cbcf7910de496beb1b7bd5c74cb
siddhik-reddy <siddhikreddy440@gmail.com>
2025-10-04 21:12:07 +0530

    add new option

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express(); // creats express application
 app.use(express.json()); //json parser
 
 app.get('/', (req,res) => {
-    res.send('hello welcome to express');
+    res.send('hello welcome to express\n 1.select one');
 });
 
 app.listen(4000,() =>
